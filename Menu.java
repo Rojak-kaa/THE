a209401050ac69bdfdb5e8f2adac1a3a6fac8f14
hebe-foo <heeeboof@gmail.com>
2025-11-22 16:19:31 +0800

    Menu.java

---
+++ Menu.java
@@ -0,0 +1,20 @@
+public class Menu
+{
+    private Inventory inventory; //connect to Inventory
+
+    public Menu(Inventory inv) {
+        this.inventory = inv;
+    }
+
+    //Display Menu (Available Items)
+    public void displayMenu() {
+        System.out.println("----- Menu -----");
+
+        for(BakeryItem item : inventory.getItems()) {
+            if(item.getQuantity() > 0) {
+                System.out.println("Item ID: " + item.getId());
+                System.out.println("Name: " + item.getName());
+                System.out.println("Price: " + item.getPrice());
+            }
+        }
+}
